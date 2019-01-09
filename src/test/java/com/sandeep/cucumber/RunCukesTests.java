519035cbeafdb25da6f18b31f87467720a30785e
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-09 18:14:39 +0530

    Renamed the java file to enable it to run from maven & switched to TestNG instead of JUnit

---
+++ src/test/java/com/sandeep/cucumber/RunCukesTests.java
@@ -0,0 +1,72 @@
+package com.sandeep.cucumber;
+
+import com.sandeep.config.FrameworkConfig;
+import com.sandeep.cucumber.context.TestContext;
+import com.sandeep.cucumber.enums.Context;
+import com.sandeep.driver.WebDriverFactory;
+import com.sandeep.pages.LoginPage;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+
+import java.util.Properties;
+
+@CucumberOptions(
+        features = "src/main/resources/features",
+        glue = {"com.sandeep.cucumber.stepdefinitions"},
+        /*tags = {"@P1"},*/
+        plugin = {
+                "pretty", "html:target/cucumber-reports",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "json:target/cucumber-reports/Cucumber.json",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        monochrome = true
+)
+@Slf4j
+public class RunCukesTests extends AbstractTestNGCucumberTests {
+    private TestContext testContext;
+    private WebDriverFactory driverFactory;
+    private String driver_type;
+    private String browser;
+    private Properties config;
+    private WebDriver driver;
+    private String environment_url;
+
+    @BeforeClass
+    public void setup() {
+        initialize_runner_elements();
+        environment_url = launch_browser();
+        testContext.getScenarioContext().setContext("url", environment_url);
+    }
+
+    @AfterClass
+    public void tearDown () {
+        driverFactory.closeDriver();
+    }
+
+    @DataProvider(parallel = true)
+    @Override
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
+    private void initialize_runner_elements () {
+        config = FrameworkConfig.getInstance().getConfigProperties();
+        testContext = new TestContext();
+        driverFactory = WebDriverFactory.getInstance();
+        driver_type = System.getProperty("driverType", config.getProperty("DRIVERTYPE"));
+        browser = System.getProperty("browser", config.getProperty("BROWSER"));
+    }
+
+    private String launch_browser () {
+        log.info("Driver {} created for browser {}", driver_type.toUpperCase(), browser.toUpperCase());
+        driver = driverFactory.getDriver(driver_type);
+        driver.manage().window().maximize();
+        return config.getProperty(System.getProperty("env", "dev")
+                                          .equalsIgnoreCase("dev")?"url_dev":"url_qa");
+    }
+}
