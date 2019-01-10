c4137a217b564d1c40cfb4dcba85f003010dfb47
Sandeep Singh <s.sandeep79@ymail.com>
2019-01-10 10:02:25 +0530

    cleaned up code removing methods not overriden

--- src/main/java/com/sandeep/listeners/ScreenshotListener.java
+++ src/main/java/com/sandeep/listeners/ScreenshotListener.java
@@ -1,54 +1,41 @@
 package com.sandeep.listeners;
 
+import com.sandeep.config.FrameworkConfig;
+import com.sandeep.driver.WebDriverFactory;
 import com.sandeep.util.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.TestListenerAdapter;
 
+import java.util.Arrays;
+
 @Slf4j
 public class ScreenshotListener extends TestListenerAdapter {
-    private WebDriver driver;
-    @Override
-    public void onTestSuccess(ITestResult iTestResult) {
-        super.onTestSuccess(iTestResult);
-    }
-
     @Override
     public void onTestFailure(ITestResult iTestResult) {
         super.onTestFailure(iTestResult);
-
-        driver = (WebDriver) iTestResult.getTestContext().getAttribute("driver");
-        try {
-            Utils.take_screenshot(driver, iTestResult);
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onTestSkipped(ITestResult iTestResult) {
-        super.onTestSkipped(iTestResult);
-    }
-
-    @Override
-    public void onStart(ITestContext iTestContext) {
-        super.onStart(iTestContext);
-    }
-
-    @Override
-    public void onFinish(ITestContext iTestContext) {
-        super.onFinish(iTestContext);
-    }
-
-    @Override
-    public void onTestStart(ITestResult iTestResult) {
-        super.onTestStart(iTestResult);
+        take_screenshot(iTestResult);
     }
 
     @Override
     public void onConfigurationFailure(ITestResult iTestResult) {
         super.onConfigurationFailure(iTestResult);
+        take_screenshot(iTestResult);
+    }
+
+    private void take_screenshot (ITestResult iTestResult) {
+        WebDriver driver = (WebDriver) iTestResult.getTestContext().getAttribute("driver");
+        try {
+            Utils.take_screenshot(driver, iTestResult);
+        } catch (NullPointerException e) {
+            log.error("encountered NPE while taking a screenshot!!");
+            log.error("{}", e.getCause());
+            log.error("{}", Arrays.toString(e.getStackTrace()));
+
+            driver = WebDriverFactory.getInstance().getDriver(System.getProperty("driverType",
+                    FrameworkConfig.getInstance().getConfigProperties().getProperty("DRIVERTYPE")));
+            Utils.take_screenshot(driver, iTestResult);
+        }
     }
 }
