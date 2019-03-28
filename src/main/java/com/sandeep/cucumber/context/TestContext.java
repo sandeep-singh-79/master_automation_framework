3def9caf3bab08f6abc0c168147f4479cf7ef2e8
Sandeep Singh <s.sandeep79@ymail.com>
2019-03-28 19:18:33 +0530

    added some generic use objects to BaseStepDefinition class, and TestContext

--- src/main/java/com/sandeep/cucumber/context/TestContext.java
+++ src/main/java/com/sandeep/cucumber/context/TestContext.java
@@ -1,14 +1,23 @@
 package com.sandeep.cucumber.context;
 
+import com.sandeep.config.FrameworkConfig;
 import com.sandeep.driver.WebDriverFactory;
+
+import java.util.Properties;
 
 public class TestContext {
     private WebDriverFactory webDriverManager;
     private ScenarioContext scenarioContext;
+    private Properties config;
 
     public TestContext() {
         webDriverManager = WebDriverFactory.getInstance();
         scenarioContext = ScenarioContext.getInstance();
+        config = FrameworkConfig.getInstance().getConfigProperties();
+    }
+
+    public Properties getConfig () {
+        return config;
     }
 
     public WebDriverFactory getWebDriverManager() {
--- src/test/java/com/sandeep/base/cucumber/BaseStepDefinition.java
+++ src/test/java/com/sandeep/base/cucumber/BaseStepDefinition.java
@@ -1,15 +1,32 @@
 package com.sandeep.base.cucumber;
 
+import com.sandeep.config.PropertyReader;
 import com.sandeep.cucumber.context.TestContext;
 import com.sandeep.cucumber.enums.Context;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.Properties;
 
 public class BaseStepDefinition {
-    protected WebDriver driver;
     protected TestContext context;
+    protected WebDriver driver;
+    protected Properties test_data;
+    protected Properties config;
+    protected Actions mouseOver;
+    protected JavascriptExecutor js;
+    protected String envir;
 
     public BaseStepDefinition (TestContext context) {
         this.context = context;
         driver = (WebDriver) context.getScenarioContext().getContext(Context.DRIVER.toString());
+
+        test_data = new PropertyReader(new java.io.File(String.format("%s/src/test/resources/test_data/data.properties", System.getProperty("user.dir"))))
+                .getPropertyFile();
+        config = this.context.getConfig();
+        mouseOver = new Actions(driver);
+        js = (JavascriptExecutor) driver;
+        envir = System.getProperty("env", config.getProperty("env"));
     }
 }
