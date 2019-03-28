c5338167c51d362b391e485f07608c96fc5e40c7
Sandeep Singh <s.sandeep79@ymail.com>
2019-03-28 19:19:26 +0530

    added further general use objects to Base API test class

--- src/test/java/com/sandeep/base/BaseAPITest.java
+++ src/test/java/com/sandeep/base/BaseAPITest.java
@@ -2,12 +2,16 @@ package com.sandeep.base;
 
 import com.sandeep.base.api.ApiBase;
 import com.sandeep.config.FrameworkConfig;
+import com.sandeep.config.PropertyReader;
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
 import java.util.Properties;
 
 @Slf4j
@@ -15,6 +19,11 @@ public abstract class BaseAPITest {
     protected Properties config = FrameworkConfig.getInstance().getConfigProperties();
     protected ApiBase apiBase;
     protected RequestSpecification requestSpec;
+    protected Properties test_data_props = new PropertyReader(new File(System.getProperty("user.dir") + "/src/test/resources/test_data/api_test_data.properties")).getPropertyFile();
+    protected Response response;
+    protected String base_url;
+    protected byte base_port;
+    protected Headers headers;
 
     @BeforeClass
     public void setup(){
